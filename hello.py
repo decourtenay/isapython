91fa56c93c9f6a15804f7322432fcc98cca5aced
nowakpm <nowakpm@gmail.com>
2018-09-13 18:50:23 +0200

    First commit

--- hello.py
+++ hello.py
@@ -6,3 +6,13 @@ number = 3
 number_str = "3"
 
 print(type(number))
+
+hello = "Hello"
+world = "world"
+ex = "!"
+
+sign = hello + " " + world + " " + ex
+print(sign)
+
+name = input("Jak masz na imię? ")
+print(hello + " " + name + ex)
